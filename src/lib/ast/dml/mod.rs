bdf69dac1078cbd0c8635e0233ff3226b33f3c73
myyrakle <sssang97@naver.com>
2022-02-27 03:00:20 +0900

    add: dml, dcl 구조 추가

---
+++ src/lib/ast/dml/mod.rs
@@ -0,0 +1,9 @@
+pub mod delete;
+pub mod insert;
+pub mod select;
+pub mod update;
+
+pub use delete::*;
+pub use insert::*;
+pub use select::*;
+pub use update::*;
--- src/lib/ast/mod.rs
+++ src/lib/ast/mod.rs
@@ -1,8 +1,14 @@
 pub mod traits;
 pub use traits::*;
 
+pub mod types;
+pub use types::*;
+
 pub mod ddl;
 pub use ddl::*;
 
-pub mod types;
-pub use types::*;
+pub mod dml;
+pub use dml::*;
+
+pub mod dcl;
+pub use dcl::*;
