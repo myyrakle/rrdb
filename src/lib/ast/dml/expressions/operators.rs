8194e7f7a71169e4a820fc02447a2a79c531dce0
myyrakle <sssang97@naver.com>
2022-03-08 01:32:52 +0900

    add: Like operator

--- src/lib/ast/dml/expressions/operators.rs
+++ src/lib/ast/dml/expressions/operators.rs
@@ -1,17 +1,18 @@
 #[derive(Clone, Debug)]
 pub enum BinaryOperator {
-    Add, // A + B
-    Sub, // A - B
-    Mul, // A * B
-    Div, // A / B
-    And, // A AND B
-    Or,  // A Or B
-    Lt,  // A < B
-    Gt,  // A > B
-    Lte, // A <= B
-    Gte, // A >= B
-    Eq,  // A = B
-    Neq, // A != B, A <> B
+    Add,  // A + B
+    Sub,  // A - B
+    Mul,  // A * B
+    Div,  // A / B
+    And,  // A AND B
+    Or,   // A OR B
+    Lt,   // A < B
+    Gt,   // A > B
+    Lte,  // A <= B
+    Gte,  // A >= B
+    Eq,   // A = B
+    Neq,  // A != B, A <> B
+    Like, // A LIKE B
 }
 
 #[derive(Clone, Debug)]
