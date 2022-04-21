05fce7d5a4b8f753e5540ad18e033d9911aee3a5
myyrakle <sssang97@naver.com>
2022-04-22 00:08:53 +0900

    fix: 문서 개선

--- src/lib/ast/ddl/create_table.rs
+++ src/lib/ast/ddl/create_table.rs
@@ -4,8 +4,8 @@ use crate::lib::{ForeignKey, Table, TableOptions, UniqueKey};
 
 /*
 CREATE TABLE [IF NOT EXISTS] [database_name.]table_name (
-    column_name data_type [NOT NULL | NULL],
-    column_name data_type [NOT NULL | NULL],
+    column_name data_type [NOT NULL | NULL] [PRIMARY KEY] [COMMENT 'comment'],
+    column_name data_type [NOT NULL | NULL] [PRIMARY KEY] [COMMENT 'comment'],
     ...
     PRIMARY KEY (column_name),
     UNIQUE (column_name),
@@ -27,7 +27,7 @@ pub struct CreateTableQuery {
 }
 
 impl CreateTableQuery {
-    pub fn builder()->Self {
+    pub fn builder() -> Self {
         CreateTableQuery {
             table: None,
             columns: vec![],
@@ -35,42 +35,41 @@ impl CreateTableQuery {
             foreign_keys: vec![],
             unique_keys: vec![],
             table_options: None,
-            if_not_exists:false,
+            if_not_exists: false,
         }
     }
 
-    pub fn set_table<'a>(&'a mut self, table: Table) -> &'a mut Self {    
+    pub fn set_table<'a>(&'a mut self, table: Table) -> &'a mut Self {
         self.table = Some(table);
         self
     }
 
-    pub fn set_table_option<'a>(&'a mut self, option: TableOptions) -> &'a mut Self {    
+    pub fn set_table_option<'a>(&'a mut self, option: TableOptions) -> &'a mut Self {
         self.table_options = Some(option);
         self
     }
 
-
-    pub fn add_column<'a>(&'a mut self, column: Column) -> &'a mut Self {    
+    pub fn add_column<'a>(&'a mut self, column: Column) -> &'a mut Self {
         self.columns.push(column);
         self
     }
 
-    pub fn set_primary_key<'a>(&'a mut self, columns: Vec<String>) -> &'a mut Self {    
+    pub fn set_primary_key<'a>(&'a mut self, columns: Vec<String>) -> &'a mut Self {
         self.primary_key = columns;
         self
     }
 
-    pub fn add_unique_key<'a>(&'a mut self, unique_key: UniqueKey) -> &'a mut Self {    
+    pub fn add_unique_key<'a>(&'a mut self, unique_key: UniqueKey) -> &'a mut Self {
         self.unique_keys.push(unique_key);
         self
     }
 
-    pub fn set_if_not_exists<'a>(&'a mut self, if_not_exists: bool) -> &'a mut Self {    
+    pub fn set_if_not_exists<'a>(&'a mut self, if_not_exists: bool) -> &'a mut Self {
         self.if_not_exists = if_not_exists;
         self
     }
 
-    pub fn build(self)->Box<dyn SQLStatement> {
+    pub fn build(self) -> Box<dyn SQLStatement> {
         Box::new(self)
     }
 }
