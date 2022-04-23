5ed80a0fd1507bfdb9f65211b82a6993b1df7220
myyrakle <sssang97@naver.com>
2022-04-24 01:56:08 +0900

    add: ddl alter table ast 보완

--- src/lib/ast/ddl/alter_table.rs
+++ src/lib/ast/ddl/alter_table.rs
@@ -1,7 +1,16 @@
 pub use crate::lib::ast::traits::{DDLStatement, SQLStatement};
 use crate::lib::ast::types::Column;
 use crate::lib::ast::types::Table;
+use crate::lib::DataType;
 
+/*
+ALTER TABLE [database_name.]table_name
+[RENAME COLUMN from_name TO to_name]
+[ALTER COLUMN column_name TYPE]
+[DROP COLUMN column_name]
+[ADD COLUMN column_name column_type]
+;
+*/
 #[derive(Debug, Clone)]
 pub struct AlterTableQuery {
     pub table: Table,
@@ -9,6 +18,7 @@ pub struct AlterTableQuery {
     pub add_columns: Vec<AlterTableAddColumn>,
     pub alter_columns: Vec<AlterTableAlterColumn>,
     pub drop_columns: Vec<AlterTableDropColumn>,
+    pub rename_columns: Vec<AlterTableRenameColumn>,
 }
 
 impl DDLStatement for AlterTableQuery {}
@@ -33,4 +43,16 @@ pub struct AlterTableDropColumn {
 #[derive(Debug, Clone)]
 pub struct AlterTableAlterColumn {
     pub column_name: String,
+    pub set_type: Option<DataType>,
+    pub set_default: Option<()>, // ... 아직 미설계
+    pub drop_default: Option<bool>,
+    pub set_not_null: Option<bool>,
+    pub set_primary_key: Option<bool>,
+    pub set_comment: Option<String>,
+}
+
+#[derive(Debug, Clone)]
+pub struct AlterTableRenameColumn {
+    pub from_name: String,
+    pub to_name: String,
 }
