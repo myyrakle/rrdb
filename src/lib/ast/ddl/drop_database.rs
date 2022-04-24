d27af65c899423611aac1df074caddb67098999f
myyrakle <sssang97@naver.com>
2022-04-24 20:10:43 +0900

    add: drop database ast 추가

---
+++ src/lib/ast/ddl/drop_database.rs
@@ -0,0 +1,38 @@
+pub use crate::lib::ast::traits::{DDLStatement, SQLStatement};
+pub use crate::lib::ast::types::Column;
+
+/*
+DROP DATABASE [IF EXISTS] database_name;
+*/
+#[derive(Debug, Clone)]
+pub struct DropDatabaseQuery {
+    pub database_name: Option<String>,
+    pub if_exists: bool,
+}
+
+impl DropDatabaseQuery {
+    pub fn builder() -> Self {
+        DropDatabaseQuery {
+            database_name: None,
+            if_exists: false,
+        }
+    }
+
+    pub fn set_name<'a>(&'a mut self, name: String) -> &'a mut Self {
+        self.database_name = Some(name);
+        self
+    }
+
+    pub fn set_if_exists<'a>(&'a mut self, set_if_exists: bool) -> &'a mut Self {
+        self.if_exists = set_if_exists;
+        self
+    }
+
+    pub fn build(self) -> Box<dyn SQLStatement> {
+        Box::new(self)
+    }
+}
+
+impl DDLStatement for DropDatabaseQuery {}
+
+impl SQLStatement for DropDatabaseQuery {}
--- src/lib/ast/ddl/mod.rs
+++ src/lib/ast/ddl/mod.rs
@@ -7,5 +7,7 @@ pub use create_table::*;
 pub use drop_table::*;
 
 pub mod create_database;
+pub mod drop_database;
 
 pub use create_table::*;
+pub use drop_table::*;
