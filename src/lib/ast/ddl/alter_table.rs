f71f2175d4ecbd8f465f6472e156f719eebaeec6
myyrakle <sssang97@naver.com>
2022-04-24 20:04:52 +0900

    add: ddl 정리

--- src/lib/ast/ddl/alter_table.rs
+++ src/lib/ast/ddl/alter_table.rs
@@ -5,54 +5,98 @@ use crate::lib::DataType;
 
 /*
 ALTER TABLE [database_name.]table_name
-[RENAME COLUMN from_name TO to_name]
-[ALTER COLUMN column_name TYPE]
-[DROP COLUMN column_name]
-[ADD COLUMN column_name column_type]
-;
+{
+    [RENAME TO new_table_name] |
+    [RENAME COLUMN from_name TO new_name] |
+    [ALTER COLUMN column_name ...] |
+    [DROP COLUMN column_name] |
+    [ADD COLUMN column_name column_type ... ] ...
+};
 */
 #[derive(Debug, Clone)]
 pub struct AlterTableQuery {
     pub table: Table,
-    pub rename_to: Option<AlterTableRenameTo>,
-    pub add_columns: Vec<AlterTableAddColumn>,
-    pub alter_columns: Vec<AlterTableAlterColumn>,
-    pub drop_columns: Vec<AlterTableDropColumn>,
-    pub rename_columns: Vec<AlterTableRenameColumn>,
+    pub action: AlterTableAction,
+}
+
+#[derive(Debug, Clone)]
+pub enum AlterTableAction {
+    AlterTableRenameTo(AlterTableRenameTo),
+    AddColumn(AlterTableAddColumn),
+    AlterColumn(AlterTableAlterColumn),
+    DropColumn(AlterTableDropColumn),
+    RenameColumn(AlterTableRenameColumn),
 }
 
 impl DDLStatement for AlterTableQuery {}
 
 impl SQLStatement for AlterTableQuery {}
 
+// 테이블명 변경
+// ALTER TABLE [database_name.]table_name RENAME TO new_table_name;
 #[derive(Debug, Clone)]
 pub struct AlterTableRenameTo {
     pub name: String,
 }
 
-#[derive(Debug, Clone)]
-pub struct AlterTableAddColumn {
-    pub column: Column,
-}
-
-#[derive(Debug, Clone)]
-pub struct AlterTableDropColumn {
-    pub column_name: String,
-}
-
-#[derive(Debug, Clone)]
-pub struct AlterTableAlterColumn {
-    pub column_name: String,
-    pub set_type: Option<DataType>,
-    pub set_default: Option<()>, // ... 아직 미설계
-    pub drop_default: Option<bool>,
-    pub set_not_null: Option<bool>,
-    pub set_primary_key: Option<bool>,
-    pub set_comment: Option<String>,
-}
-
+// 컬럼 이름 변경
+// ALTER TABLE [database_name.]table_name RENAME COLUMN from_name TO new_name;
 #[derive(Debug, Clone)]
 pub struct AlterTableRenameColumn {
     pub from_name: String,
     pub to_name: String,
 }
+
+// 컬럼 추가
+// ALTER TABLE [database_name.]table_name ADD COLUMN column_name column_type [NOT NULL | NULL] [PRIMARY KEY] [COMMENT 'comment'];
+#[derive(Debug, Clone)]
+pub struct AlterTableAddColumn {
+    pub column: Column,
+}
+
+// 컬럼 삭제
+// ALTER TABLE [database_name.]table_name DROP COLUMN column_name;
+#[derive(Debug, Clone)]
+pub struct AlterTableDropColumn {
+    pub column_name: String,
+}
+
+// 컬럼 변경
+// ALTER COLUMN column_name [TYPE type_name] [{SET | DROP} NOT NULL] [{SET | DROP} DEFAULT default_expr] [{SET | DROP} COMMENT 'comment']
+#[derive(Debug, Clone)]
+pub struct AlterTableAlterColumn {
+    pub column_name: String,
+    pub action: AlterColumnAction,
+}
+
+#[derive(Debug, Clone)]
+pub enum AlterColumnAction {
+    AlterColumnType(DataType),
+    AlterColumnSetNotNull,
+    AlterColumnDropNotNull,
+    AlterColumnSetDefault(AlterTableSetDefault),
+    AlterColumnDropDefault(AlterTableDropDefault),
+}
+
+#[derive(Debug, Clone)]
+pub struct AlterTableSetType {
+    pub data_type: DataType,
+}
+
+#[derive(Debug, Clone)]
+pub struct AlterColumnSetNotNull {
+    pub data_type: DataType,
+}
+
+#[derive(Debug, Clone)]
+pub struct AlterColumnDropNotNull {
+    pub data_type: DataType,
+}
+
+#[derive(Debug, Clone)]
+pub struct AlterTableSetDefault {
+    pub default_expression: (),
+}
+
+#[derive(Debug, Clone)]
+pub struct AlterTableDropDefault {}
