5dedb6f02b30a8efb9bac30f76e5a7cf2df4d299
myyrakle <sssang97@naver.com>
2022-04-24 20:16:49 +0900

    add: alter database ast 추가

---
+++ src/lib/ast/ddl/alter_database.rs
@@ -0,0 +1,51 @@
+pub use crate::lib::ast::traits::{DDLStatement, SQLStatement};
+pub use crate::lib::ast::types::Column;
+
+/*
+ALTER DATABASE database_name
+{
+    RENAME TO new_database_name
+};
+*/
+#[derive(Debug, Clone)]
+pub struct AlterDatabaseQuery {
+    pub database_name: Option<String>,
+    pub action: Option<AlterDatabaseAction>,
+}
+
+impl AlterDatabaseQuery {
+    pub fn builder() -> Self {
+        AlterDatabaseQuery {
+            database_name: None,
+            action: None,
+        }
+    }
+
+    pub fn set_name<'a>(&'a mut self, name: String) -> &'a mut Self {
+        self.database_name = Some(name);
+        self
+    }
+
+    pub fn set_action<'a>(&'a mut self, action: AlterDatabaseAction) -> &'a mut Self {
+        self.action = Some(action);
+        self
+    }
+
+    pub fn build(self) -> Box<dyn SQLStatement> {
+        Box::new(self)
+    }
+}
+
+impl DDLStatement for AlterDatabaseQuery {}
+
+impl SQLStatement for AlterDatabaseQuery {}
+
+#[derive(Debug, Clone)]
+pub enum AlterDatabaseAction {
+    RenameTo(AlterDatabaseRenameTo),
+}
+
+#[derive(Debug, Clone)]
+pub struct AlterDatabaseRenameTo {
+    pub name: String,
+}
