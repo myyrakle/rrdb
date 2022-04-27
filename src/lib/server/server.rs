6db6d7dd3b90434c88627aea457b69756408fdfa
myyrakle <sssang97@naver.com>
2022-04-28 01:24:12 +0900

    add: 서버 일부 구현

--- src/lib/server/server.rs
+++ src/lib/server/server.rs
@@ -1,5 +1,8 @@
 use crate::lib::server::ServerOption;
 
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::TcpListener;
+
 pub struct Server {
     pub option: ServerOption,
 }
@@ -9,9 +12,46 @@ impl Server {
         Self { option }
     }
 
-    pub async fn run(&self) {
+    pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         println!("# Server is Running at {}", self.option.port);
 
-        loop {}
+        let listener = TcpListener::bind(format!("127.0.0.1:{}", self.option.port)).await?;
+
+        loop {
+            match listener.accept().await {
+                Ok((mut socket, address)) => {
+                    println!("# Connected: {}:{}", address.ip(), address.port());
+
+                    tokio::spawn(async move {
+                        let mut buffer = [0; 1024];
+
+                        // In a loop, read data from the socket and write the data back.
+                        loop {
+                            match socket.read(&mut buffer).await {
+                                // socket closed
+                                Ok(n) => {
+                                    if n != 0 {
+                                        let foo = String::from_utf8_lossy(&buffer);
+                                        println!("{}", foo);
+                                    }
+
+                                    // Write the data back
+                                    // if let Err(e) = socket.write_all(&buffer[0..n]).await {
+                                    //     eprintln!("failed to write to socket; err = {:?}", e);
+                                    //     return;
+                                    // }
+                                }
+                                Err(e) => {
+                                    eprintln!("failed to read from socket; err = {:?}", e);
+                                }
+                            };
+                        }
+                    });
+                }
+                Err(error) => {
+                    println!("# Error: {}", error);
+                }
+            }
+        }
     }
 }
