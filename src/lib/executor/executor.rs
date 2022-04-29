aa36cf47ee3b3de2ab46782cf739a8caefcf573e
myyrakle <sssang97@naver.com>
2022-04-29 23:07:24 +0900

    feature: init 명령 구현

--- src/lib/executor/executor.rs
+++ src/lib/executor/executor.rs
@@ -11,9 +11,11 @@ impl Executor {
     pub async fn init(&self, path: String) -> Result<(), Box<dyn std::error::Error>> {
         let mut path_buf = PathBuf::new();
         path_buf.push(path);
-
         path_buf.push(".rrdb.config");
-        (match tokio::fs::create_dir(path_buf.clone()).await {
+
+        // 루트 디렉터리 생성
+        let base_path = path_buf.clone();
+        (match tokio::fs::create_dir(base_path.clone()).await {
             Ok(_) => Ok(()),
             Err(error) => {
                 if error.kind() == std::io::ErrorKind::AlreadyExists {
@@ -24,10 +26,12 @@ impl Executor {
             }
         })?;
 
-        path_buf.push(".global.config");
+        // 전역 설정파일 생성
+        let mut global_path = base_path.clone();
+        global_path.push("global.config");
         let global_info = GlobalConfig::default();
         let global_config = toml::to_string(&global_info).unwrap();
-        tokio::fs::write(path_buf, global_config.as_bytes()).await?;
+        tokio::fs::write(global_path, global_config.as_bytes()).await?;
 
         Ok(())
     }
