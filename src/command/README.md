7657935845c43a48ef6bba9e276590a2ab4fb241
myyrakle <sssang97@naver.com>
2022-05-16 13:05:20 +0000

    add: readmes

---
+++ src/command/README.md
@@ -0,0 +1,3 @@
+## Command
+
+command line 명령어에 대한 정의입니다.
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,16 @@
 pub mod command;
 pub mod lib;
 
-use command::commands::SubCommand;
+use command::commands::{Command, SubCommand};
 use lib::constants::server::{DEFAULT_HOST, DEFAULT_PORT};
 use lib::executor::Executor;
 use lib::server::{Server, ServerOption};
 
 use clap::Parser;
 
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(subcommand)]
-    action: SubCommand,
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
+    let args = Command::parse();
 
     match args.action {
         SubCommand::Init(init) => {
