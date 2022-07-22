21b8263d24601056be17e9404e0f7623cc0ffa1a
myyrakle <sssang97@naver.com>
2022-07-23 01:26:29 +0900

    add: 테스트케이스 추가

--- src/lib/parser/test/expressions.rs
+++ src/lib/parser/test/expressions.rs
@@ -8,6 +8,34 @@ use crate::lib::parser::predule::Parser;
 #[test]
 pub fn expression_1() {
     let text = r#"
+        SELECT 3 + 5 AS foo
+    "#
+    .to_owned();
+
+    let mut parser = Parser::new(text).unwrap();
+
+    let expected = SelectQuery::builder()
+        .add_select_item(
+            SelectItem::builder()
+                .set_item(SQLExpression::Binary(
+                    BinaryOperatorExpression {
+                        operator: BinaryOperator::Add,
+                        lhs: SQLExpression::Integer(3),
+                        rhs: SQLExpression::Integer(5),
+                    }
+                    .into(),
+                ))
+                .set_alias("foo".into())
+                .build(),
+        )
+        .build();
+
+    assert_eq!(parser.parse().unwrap(), vec![expected],);
+}
+
+#[test]
+pub fn expression_2() {
+    let text = r#"
         SELECT 2 * (3 + 5) AS foo
     "#
     .to_owned();
