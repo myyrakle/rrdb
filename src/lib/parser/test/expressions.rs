82a9a27118e1d2129dc15573bca12088c9b8c6d5
myyrakle <sssang97@naver.com>
2022-07-23 01:30:16 +0900

    add: 테스트케이스 추가

--- src/lib/parser/test/expressions.rs
+++ src/lib/parser/test/expressions.rs
@@ -36,7 +36,7 @@ pub fn expression_1() {
 #[test]
 pub fn expression_2() {
     let text = r#"
-        SELECT 2 * (3 + 5) AS foo
+        SELECT 1 + 2 + 3 AS foo
     "#
     .to_owned();
 
@@ -47,16 +47,16 @@ pub fn expression_2() {
             SelectItem::builder()
                 .set_item(SQLExpression::Binary(
                     BinaryOperatorExpression {
-                        operator: BinaryOperator::Mul,
-                        lhs: SQLExpression::Integer(2),
-                        rhs: SQLExpression::Binary(
+                        operator: BinaryOperator::Add,
+                        lhs: SQLExpression::Binary(
                             BinaryOperatorExpression {
                                 operator: BinaryOperator::Add,
-                                lhs: SQLExpression::Integer(3),
-                                rhs: SQLExpression::Integer(5),
+                                lhs: SQLExpression::Integer(1),
+                                rhs: SQLExpression::Integer(2),
                             }
                             .into(),
                         ),
+                        rhs: SQLExpression::Integer(3),
                     }
                     .into(),
                 ))
@@ -67,3 +67,38 @@ pub fn expression_2() {
 
     assert_eq!(parser.parse().unwrap(), vec![expected],);
 }
+
+// #[test]
+// pub fn expression_2() {
+//     let text = r#"
+//         SELECT 2 * (3 + 5) AS foo
+//     "#
+//     .to_owned();
+
+//     let mut parser = Parser::new(text).unwrap();
+
+//     let expected = SelectQuery::builder()
+//         .add_select_item(
+//             SelectItem::builder()
+//                 .set_item(SQLExpression::Binary(
+//                     BinaryOperatorExpression {
+//                         operator: BinaryOperator::Mul,
+//                         lhs: SQLExpression::Integer(2),
+//                         rhs: SQLExpression::Binary(
+//                             BinaryOperatorExpression {
+//                                 operator: BinaryOperator::Add,
+//                                 lhs: SQLExpression::Integer(3),
+//                                 rhs: SQLExpression::Integer(5),
+//                             }
+//                             .into(),
+//                         ),
+//                     }
+//                     .into(),
+//                 ))
+//                 .set_alias("foo".into())
+//                 .build(),
+//         )
+//         .build();
+
+//     assert_eq!(parser.parse().unwrap(), vec![expected],);
+// }
