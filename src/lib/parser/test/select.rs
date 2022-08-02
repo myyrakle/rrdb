186c0d5bcf58da8af4a340090599f755cab9ec5d
myyrakle <sssang97@naver.com>
2022-08-03 02:29:11 +0900

    add: 테스트케이스 추가

---
+++ src/lib/parser/test/select.rs
@@ -0,0 +1,30 @@
+#![cfg(test)]
+
+use crate::lib::ast::predule::{SQLExpression, SelectItem, SelectQuery, TableName};
+use crate::lib::parser::predule::Parser;
+
+#[test]
+pub fn select_from_1() {
+    let text = r#"
+        SELECT 1 as asdf
+        FROM foo.bar
+    "#
+    .to_owned();
+
+    let mut parser = Parser::new(text).unwrap();
+
+    let expected = SelectQuery::builder()
+        .add_select_item(
+            SelectItem::builder()
+                .set_item(SQLExpression::Integer(1).into())
+                .set_alias("asdf".into())
+                .build(),
+        )
+        .set_from_table(TableName {
+            database_name: Some("foo".into()),
+            table_name: "bar".into(),
+        })
+        .build();
+
+    assert_eq!(parser.parse().unwrap(), vec![expected],);
+}
