0cd6ab1bc2e0523d23705de90f57c9335b1f078a
myyrakle <sssang97@naver.com>
2022-08-17 01:06:29 +0900

    add: 테스트케이스 추가

--- src/lib/parser/test/insert.rs
+++ src/lib/parser/test/insert.rs
@@ -1,6 +1,8 @@
 #![cfg(test)]
 
-use crate::lib::ast::predule::{InsertQuery, InsertValue, SQLExpression, TableName};
+use crate::lib::ast::predule::{
+    InsertQuery, InsertValue, SQLExpression, SelectColumn, SelectItem, SelectQuery, TableName,
+};
 use crate::lib::parser::predule::Parser;
 
 #[test]
@@ -63,6 +65,51 @@ pub fn insert_into_values_2() {
                 ],
             },
         ])
+        .build();
+
+    assert_eq!(parser.parse().unwrap(), vec![expected.into()],);
+}
+
+#[test]
+pub fn insert_into_select_1() {
+    let text = r#"
+        INSERT INTO foo.bar(a, b, c)
+        Select s.a, s.b, s.c from boom.some as s
+    "#
+    .to_owned();
+
+    let mut parser = Parser::new(text).unwrap();
+
+    let expected = InsertQuery::builder()
+        .set_into_table(TableName {
+            database_name: Some("foo".into()),
+            table_name: "bar".into(),
+        })
+        .set_columns(vec!["a".into(), "b".into(), "c".into()])
+        .set_select(
+            SelectQuery::builder()
+                .add_select_item(
+                    SelectItem::builder()
+                        .set_item(SelectColumn::new(Some("s".into()), "a".into()).into())
+                        .build(),
+                )
+                .add_select_item(
+                    SelectItem::builder()
+                        .set_item(SelectColumn::new(Some("s".into()), "b".into()).into())
+                        .build(),
+                )
+                .add_select_item(
+                    SelectItem::builder()
+                        .set_item(SelectColumn::new(Some("s".into()), "c".into()).into())
+                        .build(),
+                )
+                .set_from_table(TableName {
+                    database_name: Some("boom".into()),
+                    table_name: "some".into(),
+                })
+                .set_from_alias("s".into())
+                .build(),
+        )
         .build();
 
     assert_eq!(parser.parse().unwrap(), vec![expected.into()],);
