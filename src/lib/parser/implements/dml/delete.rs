1b44330a3ed5442648425f12ada8b05a89395cf5
myyrakle <sssang97@naver.com>
2022-08-17 13:59:32 +0900

    add: delete에 where절 파싱 추가

--- src/lib/parser/implements/dml/delete.rs
+++ src/lib/parser/implements/dml/delete.rs
@@ -41,9 +41,19 @@ impl Parser {
 
         let mut query_builder = DeleteQuery::builder();
 
+        if !self.has_next_token() {
+            return Err(ParsingError::boxed("E0505 need more tokens"));
+        }
+
         // 테이블명 파싱
         let table_name = self.parse_table_name()?;
         query_builder = query_builder.set_from_table(table_name);
+
+        // WHERE 절 파싱
+        if self.next_token_is_where() {
+            let where_clause = self.parse_where(context)?;
+            query_builder = query_builder.set_where(where_clause);
+        }
 
         Ok(query_builder.build())
     }
