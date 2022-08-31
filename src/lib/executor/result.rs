38ef7f8910e24b7c0658a98c334fb0abdaec3286
myyrakle <sssang97@naver.com>
2022-08-31 18:25:34 +0900

    add: ExecuteResult 타입 보완

--- src/lib/executor/result.rs
+++ src/lib/executor/result.rs
@@ -1,11 +1,14 @@
+#[derive(Clone, Debug, PartialEq)]
 pub struct ExecuteResult {
     pub rows: Vec<ExecuteRow>,
 }
 
+#[derive(Clone, Debug, PartialEq)]
 pub struct ExecuteRow {
     pub columns: Vec<ExecuteColumn>,
 }
 
+#[derive(Clone, Debug, PartialEq)]
 pub enum ExecuteColumn {
     Bool(bool),
     Integer(i128),
