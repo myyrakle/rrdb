3db6fc4915eb73a1d5760fadc4818338e235ac74
myyrakle <sssang97@naver.com>
2022-09-11 16:52:10 +0900

    add: ParserContext에 default_database 추가

--- src/lib/parser/context.rs
+++ src/lib/parser/context.rs
@@ -1,7 +1,8 @@
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Default, Clone)]
 pub struct ParserContext {
     pub in_between_clause: bool,
     pub in_parentheses: bool,
+    pub default_database: String,
 }
 
 impl ParserContext {
@@ -12,6 +13,11 @@ impl ParserContext {
 
     pub fn set_in_parentheses(mut self, in_parentheses: bool) -> Self {
         self.in_parentheses = in_parentheses;
+        self
+    }
+
+    pub fn set_default_database(mut self, default_database: String) -> Self {
+        self.default_database = default_database;
         self
     }
 }
