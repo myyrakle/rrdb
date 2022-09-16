f437f9218b300537fbe85ad393f9cf1257aa4f30
myyrakle <sssang97@naver.com>
2022-09-16 17:34:14 +0900

    add: Logger ingo 구현

--- src/lib/logger/logger.rs
+++ src/lib/logger/logger.rs
@@ -6,4 +6,8 @@ impl Logger {
     pub fn error(text: impl Into<String>) {
         println!("{}", format!("!!ERROR: {}", text.into()).red());
     }
+
+    pub fn info(text: impl Into<String>) {
+        println!("{}", format!("@@INFO: {}", text.into()).green());
+    }
 }
