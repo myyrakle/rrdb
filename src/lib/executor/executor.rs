27d90308af5472f0df88eee0a0e52546cd6c2650
myyrakle <sssang97@naver.com>
2022-09-17 20:57:57 +0900

    add: drop table 동작 구현

--- src/lib/executor/executor.rs
+++ src/lib/executor/executor.rs
@@ -83,6 +83,7 @@ impl Executor {
             SQLStatement::DDL(DDLStatement::CreateTableQuery(query)) => {
                 self.create_table(query).await
             }
+            SQLStatement::DDL(DDLStatement::DropTableQuery(query)) => self.drop_table(query).await,
             SQLStatement::Other(OtherStatement::ShowDatabases(query)) => {
                 self.show_databases(query).await
             }
