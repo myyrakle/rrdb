1418d917742d6bba62528d5a2f70bc9408e155a5
myyrakle <sssang97@naver.com>
2022-09-18 20:00:01 +0900

    add: alter table rename to 파서 테스트케이스 추가

--- src/lib/parser/test/alter_table.rs
+++ src/lib/parser/test/alter_table.rs
@@ -1,22 +1,27 @@
 #![cfg(test)]
-use crate::lib::ast::ddl::{AlterDatabaseAction, AlterDatabaseQuery, AlterDatabaseRenameTo};
+use crate::lib::ast::ddl::{
+    AlterDatabaseAction, AlterDatabaseQuery, AlterDatabaseRenameTo, AlterTableQuery,
+    AlterTableRenameTo,
+};
+use crate::lib::ast::predule::TableName;
 use crate::lib::parser::context::ParserContext;
 use crate::lib::parser::predule::Parser;
 
 #[test]
 pub fn alter_table_rename_1() {
     let text = r#"
-        ALTER DATABASE foo RENAME TO bar;
+        ALTER TABLE foo RENAME TO bar;
     "#
     .to_owned();
 
     let mut parser = Parser::new(text).unwrap();
 
-    let expected = AlterDatabaseQuery::builder()
-        .set_name("foo".to_owned())
-        .set_action(AlterDatabaseAction::RenameTo(AlterDatabaseRenameTo {
-            name: "bar".into(),
-        }))
+    let expected = AlterTableQuery::builder()
+        .set_table(TableName {
+            table_name: "foo".to_owned(),
+            database_name: None,
+        })
+        .set_action(AlterTableRenameTo { name: "bar".into() }.into())
         .build();
 
     assert_eq!(
