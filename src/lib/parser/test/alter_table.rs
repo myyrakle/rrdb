853aa09950138790a07acca0c3daf137829f314a
myyrakle <sssang97@naver.com>
2022-09-18 23:04:57 +0900

    add: alter column set default 테스트케이스 추가

--- src/lib/parser/test/alter_table.rs
+++ src/lib/parser/test/alter_table.rs
@@ -1,10 +1,12 @@
 #![cfg(test)]
 use crate::lib::ast::ddl::{
-    AlterColumnDropNotNull, AlterColumnSetNotNull, AlterColumnSetType, AlterTableAlterColumn,
-    AlterTableDropColumn, AlterTableRenameColumn,
+    AlterColumnDropNotNull, AlterColumnSetDefault, AlterColumnSetNotNull, AlterColumnSetType,
+    AlterTableAlterColumn, AlterTableDropColumn, AlterTableRenameColumn,
 };
+use crate::lib::ast::dml::IntegerExpression;
 use crate::lib::ast::predule::{
-    AlterTableAddColumn, AlterTableQuery, AlterTableRenameTo, Column, DataType, TableName,
+    AlterTableAddColumn, AlterTableQuery, AlterTableRenameTo, Column, DataType, SQLExpression,
+    TableName,
 };
 use crate::lib::parser::predule::{Parser, ParserContext};
 
@@ -260,6 +262,38 @@ pub fn alter_table_alter_column_set_type_2() {
                 column_name: "name".into(),
                 action: AlterColumnSetType {
                     data_type: DataType::Int,
+                }
+                .into(),
+            }
+            .into(),
+        )
+        .build();
+
+    assert_eq!(
+        parser.parse(ParserContext::default()).unwrap(),
+        vec![expected],
+    );
+}
+
+#[test]
+pub fn alter_table_alter_column_set_default_1() {
+    let text = r#"
+        ALTER TABLE foo ALTER COLUMN id SET DEFAULT 0;
+    "#
+    .to_owned();
+
+    let mut parser = Parser::new(text).unwrap();
+
+    let expected = AlterTableQuery::builder()
+        .set_table(TableName {
+            table_name: "foo".to_owned(),
+            database_name: None,
+        })
+        .set_action(
+            AlterTableAlterColumn {
+                column_name: "id".into(),
+                action: AlterColumnSetDefault {
+                    expression: SQLExpression::Integer(0),
                 }
                 .into(),
             }
