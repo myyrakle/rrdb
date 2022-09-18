c979668f2e6f583337d3ece44eb2d34e88a7b0c3
myyrakle <sssang97@naver.com>
2022-09-18 21:17:02 +0900

    add: table alter rename column 테스트 케이스 추가

--- src/lib/parser/test/alter_table.rs
+++ src/lib/parser/test/alter_table.rs
@@ -87,3 +87,32 @@ pub fn alter_table_rename_column_1() {
         vec![expected],
     );
 }
+
+#[test]
+pub fn alter_table_rename_column_2() {
+    let text = r#"
+        ALTER TABLE foo RENAME name TO name_1;
+    "#
+    .to_owned();
+
+    let mut parser = Parser::new(text).unwrap();
+
+    let expected = AlterTableQuery::builder()
+        .set_table(TableName {
+            table_name: "foo".to_owned(),
+            database_name: None,
+        })
+        .set_action(
+            AlterTableRenameColumn {
+                from_name: "name".into(),
+                to_name: "name_1".into(),
+            }
+            .into(),
+        )
+        .build();
+
+    assert_eq!(
+        parser.parse(ParserContext::default()).unwrap(),
+        vec![expected],
+    );
+}
