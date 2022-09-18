f2a367c7bfc4c1d7df02dec233d45837ec244113
myyrakle <sssang97@naver.com>
2022-09-18 21:22:36 +0900

    add: alter table add column 테스트케이스 추가

--- src/lib/parser/test/alter_table.rs
+++ src/lib/parser/test/alter_table.rs
@@ -60,6 +60,37 @@ pub fn alter_table_add_column_1() {
 }
 
 #[test]
+pub fn alter_table_add_column_2() {
+    let text = r#"
+        ALTER TABLE foo ADD name varchar(100);
+    "#
+    .to_owned();
+
+    let mut parser = Parser::new(text).unwrap();
+
+    let expected = AlterTableQuery::builder()
+        .set_table(TableName {
+            table_name: "foo".to_owned(),
+            database_name: None,
+        })
+        .set_action(
+            AlterTableAddColumn {
+                column: Column::builder()
+                    .set_name("name".to_owned())
+                    .set_data_type(DataType::Varchar(100))
+                    .build(),
+            }
+            .into(),
+        )
+        .build();
+
+    assert_eq!(
+        parser.parse(ParserContext::default()).unwrap(),
+        vec![expected],
+    );
+}
+
+#[test]
 pub fn alter_table_rename_column_1() {
     let text = r#"
         ALTER TABLE foo RENAME COLUMN name TO name_1;
