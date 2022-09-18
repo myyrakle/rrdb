08179c9fc38a8a1177dbe5c24f9cfcd67321d191
myyrakle <sssang97@naver.com>
2022-09-18 23:12:14 +0900

    add: alter table add column 동작 구현

--- src/lib/executor/implements/ddl/alter_table.rs
+++ src/lib/executor/implements/ddl/alter_table.rs
@@ -1,7 +1,7 @@
 use std::error::Error;
 use std::io::ErrorKind;
 
-use crate::lib::ast::ddl::{AlterTableAction, AlterTableQuery};
+use crate::lib::ast::ddl::{AlterTableAction, AlterTableQuery, Column};
 use crate::lib::ast::predule::TableName;
 use crate::lib::errors::predule::ExecuteError;
 use crate::lib::executor::encoder::StorageEncoder;
@@ -67,10 +67,55 @@ impl Executor {
                     },
                 }
             }
-            AlterTableAction::AddColumn(_action) => {}
-            AlterTableAction::AlterColumn(_action) => {}
-            AlterTableAction::DropColumn(_action) => {}
-            AlterTableAction::RenameColumn(_action) => {}
+            AlterTableAction::AddColumn(action) => {
+                // TODO: 실 데이터 목록에도 반영하기
+
+                // config data 파일 내용 변경
+                let config_path = table_path.clone().join("table.config");
+
+                let column_to_add = action.column;
+
+                match tokio::fs::read(&config_path).await {
+                    Ok(data) => {
+                        let table_config: Option<TableConfig> = encoder.decode(data.as_slice());
+
+                        match table_config {
+                            Some(mut table_config) => {
+                                if table_config.columns.contains(&column_to_add) {
+                                    return Err(ExecuteError::boxed(format!(
+                                        "already exists column {}",
+                                        column_to_add.name
+                                    )));
+                                }
+
+                                table_config.columns.push(column_to_add);
+
+                                tokio::fs::write(config_path, encoder.encode(table_config)).await?;
+                            }
+                            None => {
+                                return Err(ExecuteError::boxed("invalid config data"));
+                            }
+                        }
+                    }
+                    Err(error) => match error.kind() {
+                        ErrorKind::NotFound => {
+                            return Err(ExecuteError::boxed("table not found"));
+                        }
+                        _ => {
+                            return Err(ExecuteError::boxed(format!("{:?}", error)));
+                        }
+                    },
+                }
+            }
+            AlterTableAction::AlterColumn(_action) => {
+                // TODO: 실 데이터 목록에도 반영하기
+            }
+            AlterTableAction::DropColumn(_action) => {
+                // TODO: 실 데이터 목록에도 반영하기
+            }
+            AlterTableAction::RenameColumn(_action) => {
+                // TODO: 실 데이터 목록에도 반영하기
+            }
             AlterTableAction::None => {}
         }
 
