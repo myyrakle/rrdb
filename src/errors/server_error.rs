a201cc18ff2988961c6d382666f128fac7329aa7
myyrakle <sssang97@naver.com>
2024-03-24 11:01:09 +0900

    [#63] ServerError에 backtrace 추가

--- src/errors/server_error.rs
+++ src/errors/server_error.rs
@@ -1,12 +1,14 @@
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Debug)]
 pub struct ServerError {
     pub message: String,
+    pub backtrace: std::backtrace::Backtrace,
 }
 
 impl ServerError {
     pub fn new<T: ToString>(message: T) -> Self {
         Self {
             message: message.to_string(),
+            backtrace: std::backtrace::Backtrace::capture(),
         }
     }
 
