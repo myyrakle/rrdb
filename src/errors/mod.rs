7a7c1569a69de08030ab46023e13098f46e24bb5
myyrakle <sssang97@naver.com>
2024-03-24 10:38:18 +0900

    [#63] RRDBError 최상위 에러 타입 추가

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -5,3 +5,13 @@ pub mod parsing_error;
 pub mod predule;
 pub mod server_error;
 pub mod type_error;
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum RRDBError {
+    ExecuteError(execute_error::ExecuteError),
+    IntoError(into_error::IntoError),
+    LexingError(lexing_error::LexingError),
+    ParsingError(parsing_error::ParsingError),
+    ServerError(server_error::ServerError),
+    TypeError(type_error::TypeError),
+}
