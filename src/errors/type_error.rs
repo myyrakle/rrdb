9a3a4a359b847b30cb84d4f39f0d2ce29016b1ef
myyrakle <sssang97@naver.com>
2024-03-24 11:01:26 +0900

    [#63] TypeError에 backtrace 추가

--- src/errors/type_error.rs
+++ src/errors/type_error.rs
@@ -1,14 +1,16 @@
 use super::RRDBError;
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Debug)]
 pub struct TypeError {
     pub message: String,
+    pub backtrace: std::backtrace::Backtrace,
 }
 
 impl TypeError {
     pub fn new<T: ToString>(message: T) -> RRDBError {
         RRDBError::TypeError(Self {
             message: message.to_string(),
+            backtrace: std::backtrace::Backtrace::capture(),
         })
     }
 }
