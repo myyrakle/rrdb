b0b475c491e08ec1567a6dc2ccd12a55c86be83c
myyrakle <sssang97@naver.com>
2024-07-15 22:59:23 +0900

    [#124] macos 테스트코드 작성

--- src/utils/macos.rs
+++ src/utils/macos.rs
@@ -6,3 +6,17 @@ pub fn get_profile_path() -> String {
 
     profile_path
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_profile_path() {
+        let username = whoami::username();
+        let user_path = format!("/Users/{}", username);
+        let profile_path = format!("{}/.zshenv", user_path);
+
+        assert_eq!(get_profile_path(), profile_path);
+    }
+}
