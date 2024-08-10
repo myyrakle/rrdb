9fcfed5a902a7517188eeff2e2daa72fe593d082
myyrakle <sssang97@naver.com>
2024-08-11 03:14:53 +0900

    [#139] parse_backslash_query 테스트코드 재작성

--- src/parser/test/other.rs
+++ src/parser/test/other.rs
@@ -152,20 +152,64 @@ fn test_parse_desc_query() {
 }
 
 #[test]
-pub fn show_databases_2() {
-    let text = r#"
-        \l
-    "#
-    .to_owned();
+fn test_parse_backslash_query() {
+    struct TestCase {
+        name: String,
+        input: Vec<Token>,
+        context: ParserContext,
+        expected: SQLStatement,
+        want_error: bool,
+    }
 
-    let mut parser = Parser::with_string(text).unwrap();
+    let test_cases = vec![
+        TestCase {
+            name: "\\l".into(),
+            input: vec![Token::Identifier("l".into())],
+            context: Default::default(),
+            expected: ShowDatabasesQuery {}.into(),
+            want_error: false,
+        },
+        TestCase {
+            name: "알 수없는 식별자: \\r".into(),
+            input: vec![Token::Identifier("r".into())],
+            context: Default::default(),
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: 빈 토큰".into(),
+            input: vec![Token::Backslash],
+            context: Default::default(),
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: DELETE".into(),
+            input: vec![Token::Delete],
+            context: Default::default(),
+            expected: Default::default(),
+            want_error: true,
+        },
+    ];
 
-    let expected = ShowDatabasesQuery {};
+    for t in test_cases {
+        let mut parser = Parser::new(t.input);
 
-    assert_eq!(
-        parser.parse(ParserContext::default()).unwrap(),
-        vec![expected.into()],
-    );
+        let got = parser.parse_backslash_query(t.context);
+
+        assert_eq!(
+            got.is_err(),
+            t.want_error,
+            "{}: want_error: {}, error: {:?}",
+            t.name,
+            t.want_error,
+            got.err()
+        );
+
+        if let Ok(statements) = got {
+            assert_eq!(statements, t.expected.into(), "TC: {}", t.name);
+        }
+    }
 }
 
 #[test]
