12b5191e3d6a96512c3a9c31a41ffc4c93ce729e
myyrakle <sssang97@naver.com>
2024-08-11 02:42:48 +0900

    [#139] parse_show_query 테스트코드 재작성

--- src/parser/test/other.rs
+++ src/parser/test/other.rs
@@ -5,24 +5,59 @@ use crate::ast::other::show_databases::ShowDatabasesQuery;
 use crate::ast::other::show_tables::ShowTablesQuery;
 use crate::ast::other::use_database::UseDatabaseQuery;
 use crate::ast::types::TableName;
+use crate::ast::SQLStatement;
+use crate::lexer::tokens::Token;
 use crate::parser::context::ParserContext;
 use crate::parser::predule::Parser;
 
 #[test]
-pub fn show_databases_1() {
-    let text = r#"
-        Show Databases
-    "#
-    .to_owned();
+fn test_parse_show_query() {
+    struct TestCase {
+        name: String,
+        input: Vec<Token>,
+        context: ParserContext,
+        expected: SQLStatement,
+        want_error: bool,
+    }
 
-    let mut parser = Parser::with_string(text).unwrap();
+    let test_cases = vec![
+        TestCase {
+            name: "Show Databases".into(),
+            input: vec![Token::Databases],
+            context: Default::default(),
+            expected: ShowDatabasesQuery {}.into(),
+            want_error: false,
+        },
+        TestCase {
+            name: "Show Tables".into(),
+            input: vec![Token::Tables],
+            context: ParserContext::default().set_default_database("rrdb".into()),
+            expected: ShowTablesQuery {
+                database: "rrdb".into(),
+            }
+            .into(),
+            want_error: false,
+        },
+    ];
 
-    let expected = ShowDatabasesQuery {};
+    for t in test_cases {
+        let mut parser = Parser::new(t.input);
 
-    assert_eq!(
-        parser.parse(ParserContext::default()).unwrap(),
-        vec![expected.into()],
-    );
+        let got = parser.parse_show_query(t.context);
+
+        assert_eq!(
+            got.is_err(),
+            t.want_error,
+            "{}: want_error: {}, error: {:?}",
+            t.name,
+            t.want_error,
+            got.err()
+        );
+
+        if let Ok(statements) = got {
+            assert_eq!(statements, t.expected.into(), "TC: {}", t.name);
+        }
+    }
 }
 
 #[test]
@@ -79,27 +114,6 @@ pub fn desc_table_1() {
 
     assert_eq!(
         parser.parse(ParserContext::default()).unwrap(),
-        vec![expected.into()],
-    );
-}
-
-#[test]
-pub fn show_tables_1() {
-    let text = r#"
-        show tables;
-    "#
-    .to_owned();
-
-    let mut parser = Parser::with_string(text).unwrap();
-
-    let expected = ShowTablesQuery {
-        database: "rrdb".into(),
-    };
-
-    assert_eq!(
-        parser
-            .parse(ParserContext::default().set_default_database("rrdb".into()))
-            .unwrap(),
         vec![expected.into()],
     );
 }
