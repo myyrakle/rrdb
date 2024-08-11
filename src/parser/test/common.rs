79e4e36a5f79a867d62e320225699e15abbb3aaf
myyrakle <sssang97@naver.com>
2024-08-12 01:58:34 +0900

    [#138] parse_table_column 테스트케이스 추가

--- src/parser/test/common.rs
+++ src/parser/test/common.rs
@@ -14,7 +14,7 @@ fn test_parse_table_column() {
 
     let test_cases = vec![
         TestCase {
-            name: "id INT PRIMARY".into(),
+            name: "id INT PRIMARY KEY".into(),
             input: vec![
                 Token::Identifier("id".into()),
                 Token::Identifier("INT".into()),
@@ -32,8 +32,140 @@ fn test_parse_table_column() {
             want_error: false,
         },
         TestCase {
+            name: "오류: id INT PRIMARY".into(),
+            input: vec![
+                Token::Identifier("id".into()),
+                Token::Identifier("INT".into()),
+                Token::Primary,
+            ],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: id INT PRIMARY NULL".into(),
+            input: vec![
+                Token::Identifier("id".into()),
+                Token::Identifier("INT".into()),
+                Token::Primary,
+                Token::Null,
+            ],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "id INT NOT NULL".into(),
+            input: vec![
+                Token::Identifier("id".into()),
+                Token::Identifier("INT".into()),
+                Token::Not,
+                Token::Null,
+            ],
+            expected: Column {
+                name: "id".into(),
+                data_type: DataType::Int,
+                primary_key: false,
+                comment: "".into(),
+                not_null: true,
+                default: None,
+            },
+            want_error: false,
+        },
+        TestCase {
+            name: "id INT NULL".into(),
+            input: vec![
+                Token::Identifier("id".into()),
+                Token::Identifier("INT".into()),
+                Token::Null,
+            ],
+            expected: Column {
+                name: "id".into(),
+                data_type: DataType::Int,
+                primary_key: false,
+                comment: "".into(),
+                not_null: false,
+                default: None,
+            },
+            want_error: false,
+        },
+        TestCase {
+            name: "오류: id INT NOT".into(),
+            input: vec![
+                Token::Identifier("id".into()),
+                Token::Identifier("INT".into()),
+                Token::Not,
+            ],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: id INT NOT TABLE".into(),
+            input: vec![
+                Token::Identifier("id".into()),
+                Token::Identifier("INT".into()),
+                Token::Not,
+                Token::Table,
+            ],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "id INT COMMENT 'foo'".into(),
+            input: vec![
+                Token::Identifier("id".into()),
+                Token::Identifier("INT".into()),
+                Token::Comment,
+                Token::String("foo".into()),
+            ],
+            expected: Column {
+                name: "id".into(),
+                data_type: DataType::Int,
+                primary_key: false,
+                comment: "foo".into(),
+                not_null: false,
+                default: None,
+            },
+            want_error: false,
+        },
+        TestCase {
+            name: "오류: id INT COMMENT".into(),
+            input: vec![
+                Token::Identifier("id".into()),
+                Token::Identifier("INT".into()),
+                Token::Comment,
+            ],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: id INT COMMENT DELETE".into(),
+            input: vec![
+                Token::Identifier("id".into()),
+                Token::Identifier("INT".into()),
+                Token::Comment,
+                Token::Delete,
+            ],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: id INT DEFAULT (아직 미구현)".into(),
+            input: vec![
+                Token::Identifier("id".into()),
+                Token::Identifier("INT".into()),
+                Token::Default,
+            ],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
             name: "오류: 빈 토큰".into(),
             input: vec![],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: DELETE".into(),
+            input: vec![Token::Delete],
             expected: Default::default(),
             want_error: true,
         },
