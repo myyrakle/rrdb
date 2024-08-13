71e61dd62e70be7f6ce839370c25db079a33e218
myyrakle <sssang97@naver.com>
2024-08-13 23:21:58 +0900

    [#110] handle_alter_database_query 테스트코드 재구성

--- src/parser/test/alter_database.rs
+++ src/parser/test/alter_database.rs
@@ -2,46 +2,66 @@
 use crate::ast::ddl::alter_database::{
     AlterDatabaseAction, AlterDatabaseQuery, AlterDatabaseRenameTo,
 };
+use crate::ast::SQLStatement;
+use crate::lexer::tokens::Token;
 use crate::parser::context::ParserContext;
 use crate::parser::predule::Parser;
 
 #[test]
-pub fn alter_database_1() {
-    let text = r#"
-        ALTER DATABASE foo RENAME TO bar;
-    "#
-    .to_owned();
+fn test_handle_alter_database_query() {
+    struct TestCase {
+        name: String,
+        input: Vec<Token>,
+        expected: SQLStatement,
+        want_error: bool,
+    }
 
-    let mut parser = Parser::with_string(text).unwrap();
+    let test_cases = vec![
+        TestCase {
+            name: "ALTER DATABASE foo RENAME TO bar;".into(),
+            input: vec![
+                Token::Identifier("foo".to_owned()),
+                Token::Rename,
+                Token::To,
+                Token::Identifier("bar".to_owned()),
+                Token::SemiColon,
+            ],
+            expected: AlterDatabaseQuery::builder()
+                .set_name("foo".to_owned())
+                .set_action(AlterDatabaseAction::RenameTo(AlterDatabaseRenameTo {
+                    name: "bar".into(),
+                }))
+                .build()
+                .into(),
+            want_error: false,
+        },
+        TestCase {
+            name: "ALTER DATABASE foo".into(),
+            input: vec![Token::Identifier("foo".to_owned())],
+            expected: AlterDatabaseQuery::builder()
+                .set_name("foo".to_owned())
+                .build()
+                .into(),
+            want_error: false,
+        },
+    ];
 
-    let expected = AlterDatabaseQuery::builder()
-        .set_name("foo".to_owned())
-        .set_action(AlterDatabaseAction::RenameTo(AlterDatabaseRenameTo {
-            name: "bar".into(),
-        }))
-        .build();
+    for t in test_cases {
+        let mut parser = Parser::new(t.input);
 
-    assert_eq!(
-        parser.parse(ParserContext::default()).unwrap(),
-        vec![expected],
-    );
-}
+        let got = parser.handle_alter_database_query();
 
-#[test]
-pub fn alter_database_2() {
-    let text = r#"
-        ALTER DATABASE foo;
-    "#
-    .to_owned();
+        assert_eq!(
+            got.is_err(),
+            t.want_error,
+            "{}: want_error: {}, error: {:?}",
+            t.name,
+            t.want_error,
+            got.err()
+        );
 
-    let mut parser = Parser::with_string(text).unwrap();
-
-    let expected = AlterDatabaseQuery::builder()
-        .set_name("foo".to_owned())
-        .build();
-
-    assert_eq!(
-        parser.parse(ParserContext::default()).unwrap(),
-        vec![expected],
-    );
+        if let Ok(statements) = got {
+            assert_eq!(statements, t.expected.into(), "TC: {}", t.name);
+        }
+    }
 }
