87d050b7e19bfa788c9e417910f68ee9a78b13af
myyrakle <sssang97@naver.com>
2024-08-13 23:18:45 +0900

    [#110] handle_drop_database_query 테스트케이스 추가

--- src/parser/test/drop_database.rs
+++ src/parser/test/drop_database.rs
@@ -13,15 +13,59 @@ fn test_handle_drop_database_query() {
         want_error: bool,
     }
 
-    let test_cases = vec![TestCase {
-        name: "DROP DATABASE test_db;".into(),
-        input: vec![Token::Identifier("test_db".to_owned()), Token::SemiColon],
-        expected: DropDatabaseQuery::builder()
-            .set_name("test_db".to_owned())
-            .build()
-            .into(),
-        want_error: false,
-    }];
+    let test_cases = vec![
+        TestCase {
+            name: "DROP DATABASE test_db;".into(),
+            input: vec![Token::Identifier("test_db".to_owned()), Token::SemiColon],
+            expected: DropDatabaseQuery::builder()
+                .set_name("test_db".to_owned())
+                .build()
+                .into(),
+            want_error: false,
+        },
+        TestCase {
+            name: "DROP DATABASE test_db".into(),
+            input: vec![Token::Identifier("test_db".to_owned())],
+            expected: DropDatabaseQuery::builder()
+                .set_name("test_db".to_owned())
+                .build()
+                .into(),
+            want_error: false,
+        },
+        TestCase {
+            name: "DROP DATABASE IF EXISTS test_db;".into(),
+            input: vec![
+                Token::If,
+                Token::Exists,
+                Token::Identifier("test_db".to_owned()),
+                Token::SemiColon,
+            ],
+            expected: DropDatabaseQuery::builder()
+                .set_name("test_db".to_owned())
+                .set_if_exists(true)
+                .build()
+                .into(),
+            want_error: false,
+        },
+        TestCase {
+            name: "오류: DROP DATABASE IF EXISTS".into(),
+            input: vec![Token::If, Token::Exists],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: DROP DATABASE IF EXISTS DELETE".into(),
+            input: vec![Token::If, Token::Exists, Token::Delete],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: DROP DATABASE test_db&&".into(),
+            input: vec![Token::Identifier("test_db".to_owned()), Token::And],
+            expected: Default::default(),
+            want_error: true,
+        },
+    ];
 
     for t in test_cases {
         let mut parser = Parser::new(t.input);
