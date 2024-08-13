f9a734c1582f8b588fd9ee672482abe008054c4b
myyrakle <sssang97@naver.com>
2024-08-13 23:59:02 +0900

    [#110] handle_create_table_query 테스트코드 작성

--- src/parser/test/ddl.rs
+++ src/parser/test/ddl.rs
@@ -247,3 +247,125 @@ fn test_handle_drop_query() {
         }
     }
 }
+
+#[test]
+fn test_handle_create_table_query() {
+    struct TestCase {
+        name: String,
+        input: Vec<Token>,
+        expected: SQLStatement,
+        want_error: bool,
+    }
+
+    let test_cases = vec![
+        TestCase {
+            name: "CREATE TABLE foo(id INT PRIMARY KEY)".into(),
+            input: vec![
+                Token::Identifier("foo".to_owned()),
+                Token::LeftParentheses,
+                Token::Identifier("id".to_owned()),
+                Token::Identifier("INT".to_owned()),
+                Token::Primary,
+                Token::Key,
+                Token::RightParentheses,
+            ],
+            expected: CreateTableQuery::builder()
+                .set_table(TableName::new(None, "foo".to_owned()))
+                .add_column(
+                    Column::builder()
+                        .set_name("id".to_owned())
+                        .set_data_type(DataType::Int)
+                        .set_primary_key(true)
+                        .build(),
+                )
+                .build()
+                .into(),
+            want_error: false,
+        },
+        TestCase {
+            name: "오류: 빈 토큰".into(),
+            input: vec![],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: CREATE TABLE foo".into(),
+            input: vec![Token::Identifier("foo".to_owned())],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: CREATE TABLE foo)".into(),
+            input: vec![Token::Identifier("foo".to_owned()), Token::RightParentheses],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: CREATE TABLE foo(".into(),
+            input: vec![Token::Identifier("foo".to_owned()), Token::LeftParentheses],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: CREATE TABLE foo(id INT PRIMARY KEY".into(),
+            input: vec![
+                Token::Identifier("foo".to_owned()),
+                Token::LeftParentheses,
+                Token::Identifier("id".to_owned()),
+                Token::Identifier("INT".to_owned()),
+                Token::Primary,
+                Token::Key,
+            ],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: CREATE TABLE foo(id INT PRIMARY KEY(".into(),
+            input: vec![
+                Token::Identifier("foo".to_owned()),
+                Token::LeftParentheses,
+                Token::Identifier("id".to_owned()),
+                Token::Identifier("INT".to_owned()),
+                Token::Primary,
+                Token::Key,
+                Token::LeftParentheses,
+            ],
+            expected: Default::default(),
+            want_error: true,
+        },
+        TestCase {
+            name: "오류: CREATE TABLE foo(id INT PRIMARY KEY))".into(),
+            input: vec![
+                Token::Identifier("foo".to_owned()),
+                Token::LeftParentheses,
+                Token::Identifier("id".to_owned()),
+                Token::Identifier("INT".to_owned()),
+                Token::Primary,
+                Token::Key,
+                Token::RightParentheses,
+                Token::RightParentheses,
+            ],
+            expected: Default::default(),
+            want_error: true,
+        },
+    ];
+
+    for t in test_cases {
+        let mut parser = Parser::new(t.input);
+
+        let got = parser.handle_create_table_query(Default::default());
+
+        assert_eq!(
+            got.is_err(),
+            t.want_error,
+            "{}: want_error: {}, error: {:?}",
+            t.name,
+            t.want_error,
+            got.err()
+        );
+
+        if let Ok(statements) = got {
+            assert_eq!(statements, t.expected.into(), "TC: {}", t.name);
+        }
+    }
+}
