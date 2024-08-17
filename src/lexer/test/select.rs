4ee7a2063150fe606bd902684d85137afc78edb1
myyrakle <sssang97@naver.com>
2024-08-17 21:03:21 +0900

    [#155] != 연산자 테스트케이스 추가

--- src/lexer/test/select.rs
+++ src/lexer/test/select.rs
@@ -244,6 +244,17 @@ pub fn test_operators() {
             ],
         },
         TestCase {
+            name: "연산자: !=".to_owned(),
+            input: r#"SELECT 1 != 2"#.to_owned(),
+            want_error: false,
+            expected: vec![
+                Token::Select,
+                Token::Integer(1),
+                Token::Operator(OperatorToken::Neq),
+                Token::Integer(2),
+            ],
+        },
+        TestCase {
             name: "연산자: !".to_owned(),
             input: r#"SELECT ! True"#.to_owned(),
             want_error: false,
