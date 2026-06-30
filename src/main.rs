1353a6ee1d14b4f914f1c6381f1050f1205b31ba
myyrakle <sssang97@naver.com>
2026-06-30 20:41:26 +0900

    [#205] RUST_LOG 기본값을 info로 설정

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ async fn load_launch_config(base_path: Option<&PathBuf>) -> errors::Result<Launc
 
 #[tokio::main]
 async fn main() -> errors::Result<()> {
-    env_logger::init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let args = Command::parse();
 
